638974ee30456de3319518a7cbcf9a81d75f8ab4
bitch <169540526+follicallyChallenged@users.noreply.github.com>
2026-06-30 22:40:37 +0530

    completed
    collectible destroy on collision & particle effect added

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+
+    public float rotationSpeed = 1f;
+    public GameObject particleEffect;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 1, 0);
+        transform.Rotate(0, rotationSpeed, 0);
+}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.CompareTag("player"))
+        {
+            Destroy(gameObject);
+
+            Instantiate(particleEffect, transform.position, transform.rotation);
+        }
+
     }
+
 }
